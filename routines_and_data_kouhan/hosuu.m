bb44493c306c7a7afeabeab72d9a9b3b55a4647f
Yuto-Aoki <yuuto3039@gmail.com>
2019-12-16 15:07:00 +0900

    Add function to convert from bin to dec

---
+++ routines_and_data_kouhan/hosuu.m
@@ -0,0 +1,5 @@
+function num = hosuu(bin)
+
+x = string(bin.');
+x = strjoin(x, '');
+num = nbin2dec(char(x));
---
+++ routines_and_data_kouhan/twototen.m
@@ -0,0 +1,4 @@
+function num = twototen(bin)
+x = string(bin.');
+x = strjoin(x, '');
+num = bin2dec(char(x));
